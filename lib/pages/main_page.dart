f54c5d24ba2b8679ccae39d3f06c2e4531f6b902
Dominic Petriella <dpetriella@magicwrighter.com>
2022-08-23 17:02:08 -0400

    add filter provider and button functionality

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import '../themes.dart';
 import 'entry_form.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ocebot2_0/providers/auth_provider.dart';
 import 'package:ocebot2_0/components/data_graph.dart';
 import 'package:ocebot2_0/components/data_table.dart';
-import 'package:ocebot2_0/models/data.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ocebot2_0/providers/db_data_provider.dart';
+import 'package:ocebot2_0/providers/filter_provider.dart';
 
 class MainPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
           automaticallyImplyLeading: false,
@@ -47,14 +43,22 @@ class MainPage extends StatelessWidget {
       body: SafeArea(
         child: MainPageLoader(),
       ),
-      floatingActionButton: FloatingActionButton(
-        shape: const RoundedRectangleBorder(),
-        onPressed: () => showDialog(
-            barrierDismissible: false,
-            context: context,
-            builder: (BuildContext context) => EntryForm()),
-        tooltip: 'Create New Entry',
-        child: const Icon(Icons.add, color: Colors.white,),
+      floatingActionButton: Container(
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.white, width: 2)),
+        child: FloatingActionButton(
+          shape: const RoundedRectangleBorder(),
+          onPressed: () => showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (BuildContext context) => EntryForm()),
+          tooltip: 'Create New Entry',
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 40,
+          ),
+        ),
       ),
     );
   }
@@ -67,19 +71,17 @@ class MainPageLoader extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-    return ref.watch(dbDataFutureProvider).
-      when(
+    return ref.watch(dbDataFutureProvider).when(
         data: (dbData) {
           return Column(
             children: [
               Container(
-                width: MediaQuery.of(context).size.width * .85,
-                height: MediaQuery.of(context).size.height * .35,
-                child: DataGraph(
-                  data: ref.watch(dbDataProvider).length > 0 ? ref.watch(dbDataProvider) : dbData
-                )
-              ),
+                  width: MediaQuery.of(context).size.width * .85,
+                  height: MediaQuery.of(context).size.height * .35,
+                  child: DataGraph(
+                      data: ref.watch(dbDataProvider).length > 0
+                          ? ref.watch(dbDataProvider)
+                          : dbData)),
               Center(
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 12.0),
@@ -87,55 +89,104 @@ class MainPageLoader extends ConsumerWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(color: OcebotTheme.secondaryColor, offset: Offset(
-                              0, 5
-                            ))
-                          ]
-                        ),
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: OcebotTheme.accentColor,
+                              offset: Offset(0, 5))
+                        ]),
                         child: Container(
                           decoration: BoxDecoration(
-                            color: OcebotTheme.tertiaryColor,
-                            boxShadow: OcebotTheme.pixelShadow
-                          ),
-                          width: MediaQuery.of(context).size.width * .15,
+                              color: OcebotTheme.accentColor,
+                              boxShadow: OcebotTheme.pixelShadow),
+                          width: MediaQuery.of(context).size.width * .28,
                           height: MediaQuery.of(context).size.height * .05,
-                          child: Icon(
-                            Icons.remove,
-                            color: OcebotTheme.primaryColor,
-                            size: 40
+                          child: Center(
+                            child: Text(
+                              'Filter',
+                              style: GoogleFonts.vt323(
+                                  textStyle: TextStyle(
+                                      color: Colors.white, fontSize: 25)),
+                            ),
                           ),
-                        
                         ),
                       ),
                       Text(
-                        '< 5 months',
+                        '< ${ref.watch(filterMonthsIntProvider)} mos',
                         style: GoogleFonts.vt323(
-                          textStyle: TextStyle(color: OcebotTheme.primaryColor, fontSize: 25)
-                        ),
+                            textStyle: TextStyle(
+                                color: OcebotTheme.primaryColor, fontSize: 25)),
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(color: OcebotTheme.primaryColor, offset: Offset(
-                              0, 5
-                            ))
-                          ]
-                        ),
-                        child: Container(
-                        decoration: BoxDecoration(
-                          color: OcebotTheme.primaryColor,
-                          boxShadow: OcebotTheme.pixelShadow
-                        ),
-                        width: MediaQuery.of(context).size.width * .15,
-                        height: MediaQuery.of(context).size.height * .05,
-                        child: Icon(
-                          Icons.add,
-                          color: OcebotTheme.tertiaryColor,
-                          size: 40,
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Container(
+                              decoration: BoxDecoration(boxShadow: [
+                                BoxShadow(
+                                    color: OcebotTheme.secondaryColor,
+                                    offset: Offset(0, 5))
+                              ]),
+                              child: GestureDetector(
+                                onTap: () => ref
+                                            .watch(filterMonthsIntProvider
+                                                .notifier)
+                                            .state >
+                                        1
+                                    ? ref
+                                        .watch(filterMonthsIntProvider.notifier)
+                                        .state--
+                                    : ref
+                                        .watch(filterMonthsIntProvider.notifier)
+                                        .state = 1,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: OcebotTheme.tertiaryColor,
+                                      boxShadow: OcebotTheme.pixelShadow),
+                                  width:
+                                      MediaQuery.of(context).size.width * .15,
+                                  height:
+                                      MediaQuery.of(context).size.height * .05,
+                                  child: Icon(Icons.remove,
+                                      color: OcebotTheme.primaryColor,
+                                      size: 40),
+                                ),
+                              ),
+                            ),
                           ),
+                          Container(
+                            decoration: BoxDecoration(boxShadow: [
+                              BoxShadow(
+                                  color: OcebotTheme.primaryColor,
+                                  offset: Offset(0, 5))
+                            ]),
+                            child: GestureDetector(
+                              onTap: () => ref
+                                          .watch(
+                                              filterMonthsIntProvider.notifier)
+                                          .state <
+                                      6
+                                  ? ref
+                                      .watch(filterMonthsIntProvider.notifier)
+                                      .state++
+                                  : ref
+                                      .watch(filterMonthsIntProvider.notifier)
+                                      .state = 6,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: OcebotTheme.primaryColor,
+                                    boxShadow: OcebotTheme.pixelShadow),
+                                width: MediaQuery.of(context).size.width * .15,
+                                height:
+                                    MediaQuery.of(context).size.height * .05,
+                                child: Icon(
+                                  Icons.add,
+                                  color: OcebotTheme.tertiaryColor,
+                                  size: 40,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -146,14 +197,14 @@ class MainPageLoader extends ConsumerWidget {
                 child: TableHeader(),
               ),
               DataFirebaseTable(
-                data: ref.watch(dbDataProvider).length > 0 ? ref.watch(dbDataProvider) : dbData
-              )
+                  data: ref.watch(dbDataProvider).length > 0
+                      ? ref.watch(dbDataProvider)
+                      : dbData)
             ],
           );
-        }, 
+        },
         error: (err, stack) => Text("An error occurred"),
-        loading: () => Center(child: CircularProgressIndicator())
-      );
+        loading: () => Center(child: CircularProgressIndicator()));
   }
 }
 
@@ -174,38 +225,33 @@ class TableHeader extends StatelessWidget {
             child: Center(
               child: Container(
                   child: Text(
-                    'Date',
-                    style: GoogleFonts.vt323(
-                      textStyle: TextStyle(color: OcebotTheme.primaryColor, fontSize: 25)
-                    ),
-                  )
-              ),
+                'Date',
+                style: GoogleFonts.vt323(
+                    textStyle: TextStyle(
+                        color: OcebotTheme.primaryColor, fontSize: 25)),
+              )),
             ),
           ),
-          
           Expanded(
             child: Center(
               child: Container(
                   child: Text(
-                    'Weight (g)',
-                    style: GoogleFonts.vt323(
-                      textStyle: TextStyle(color: OcebotTheme.primaryColor, fontSize: 25)
-                    ),
-                  )
-              ),
+                'Weight (g)',
+                style: GoogleFonts.vt323(
+                    textStyle: TextStyle(
+                        color: OcebotTheme.primaryColor, fontSize: 25)),
+              )),
             ),
           ),
-          
           Expanded(
             child: Center(
               child: Container(
                   child: Text(
-                    '% Change',
-                    style: GoogleFonts.vt323(
-                      textStyle: TextStyle(color: OcebotTheme.primaryColor, fontSize: 25)
-                    ),
-                  )
-                ),
+                '% Change',
+                style: GoogleFonts.vt323(
+                    textStyle: TextStyle(
+                        color: OcebotTheme.primaryColor, fontSize: 25)),
+              )),
             ),
           ),
         ],
---
+++ lib/providers/filter_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ocebot2_0/models/data.dart';
+
+final filterMonthsIntProvider = StateProvider<int>((ref) => 1);
